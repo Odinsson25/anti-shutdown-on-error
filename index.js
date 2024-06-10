674b096203bf4d9223839b77ba017fb52a2505e2
Odinsson25 <64733407+Odinsson25@users.noreply.github.com>
2024-06-10 20:43:57 +0200

    Update index.js
    Added comments at the events.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 process.on("unhandledRejection", (reason, promise) => {
     console.log("Unhandled Rejection Error");
     console.log(reason, promise)
-});
+}); // Handles Unhandled Rejection Errors
 
 process.on("uncaughtException", (err, origin) => {
     console.log("Uncaught Exeception Error");
     console.log(err, origin);
-});
+}); // Handles Uncaught Excpetion Errors
